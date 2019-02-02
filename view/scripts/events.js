5ee615ec5b7c7b10e674ed7d88fb25fc6e17e98a
Oliver Leitner <never@neverslair-blog.net>
2019-02-02 23:51:59 +0100

    onclick menuentry -> event click on menuentry

---
+++ view/scripts/events.js
@@ -0,0 +1,22 @@
+/**
+ * a simple event listener specific to certain element classes
+ * TODO: split and generalize
+ * @param {*} className the name of the class to get elements by
+ * @param {*} eventType the event we are listening for
+ */
+function stateHandler(className = null, eventType = "click") {
+    document.body.addEventListener(eventType, event => {
+        if (event.target.parentElement.classList.contains(className)) {
+            const elements = document.querySelectorAll("." + className);
+            // remove active class from all li's
+            for (i = 0; i < elements.length; i++) {
+                elements[i].classList.remove("active");
+            }
+            // add active class to current li
+            event.target.parentElement.classList.add(["active"]);
+            var table = event.target.parentElement.id.split("_")[1];
+            // get the data for the body
+            loadTableToGrid(editableGrid,"data.php",table,"datatable");
+        }
+    });
+}
