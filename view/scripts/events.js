bbec43775cb467b116e91d92117688cb8af147a7
Oliver Leitner <never@neverslair-blog.net>
2019-02-13 21:58:18 +0100

    by default, activate first menu entry

--- view/scripts/events.js
+++ view/scripts/events.js
@@ -9,8 +9,10 @@ function stateHandler(className = null, activeClass = null, eventType = null) {
         if (event.target.parentElement.classList.contains(className)) {
             let elements = document.querySelectorAll("." + className);
             // remove active class from all li's
-            for (i = 0; i < elements.length; i++) {
-                elements[i].classList.remove(activeClass);
+            if (elements) {
+                for (i = 0; i < elements.length; i++) {
+                    elements[i].classList.remove(activeClass);
+                }
             }
             // add active class to current li
             event.target.parentElement.classList.add([activeClass]);
@@ -27,20 +29,26 @@ function stateHandler(className = null, activeClass = null, eventType = null) {
  * @param {*} className the name of the ul class of the menu
  * @param {*} activeClass the class that highlights the current entry
  */
-function routing(className = null, activeClass = null) {
-    if (window.location.hash) {
-        // if we got a param, add it to the request
-        let urlparam = window.location.hash;
-        param = urlparam.split('/')[1];
-        loadTableToGrid(editableGrid,'data.php',param,'datatable');
-        let elements = document.querySelectorAll("." + className);
+function routing(className = null, activeClass = null, table_name = "") {
+    // unselect currently selected menu entry, if avail
+    let elements = document.querySelectorAll("." + className);
+    if (elements) {
         for (i = 0; i < elements.length; i++) {
             elements[i].classList.remove(activeClass);
         }
-        // activate the correct menu entry
-        document.getElementById("cur_" + param).classList.add(activeClass);
+    }
+    // activate the correct menu entry
+    let table = table_name;
+    if (window.location.hash) {
+        // if we got a param, add it to the request
+        let urlparam = window.location.hash;
+        param = urlparam.split("/")[1];
+        table = param;
+        loadTableToGrid(editableGrid,"data.php",table,"datatable");
     } else {
         // fallback, if no route supplied
-        loadTableToGrid(editableGrid,'data.php','','datatable');
+        loadTableToGrid(editableGrid,"data.php",table,"datatable");
     }
+    // activate current entry
+    document.getElementById("cur_" + table).classList.add(activeClass);
 }
