884287fb4dff9a951cc9fd37635128ea0fdd2044
Oliver Leitner <never@neverslair-blog.net>
2019-02-14 21:16:51 +0100

    playing with more optimisations and gc

--- view/scripts/events.js
+++ view/scripts/events.js
@@ -7,37 +7,45 @@
 function stateHandler(className = null, activeClass = null, eventType = null) {
     document.body.addEventListener(eventType, event => {
         if (event.target.parentElement.classList.contains(className)) {
-            let elements = document.querySelectorAll("." + className);
+            let elements = document.getElementsByClassName(className);
             // remove active class from all li's
             if (elements) {
                 const arrayLength = elements.length;
                 for (i = 0; i < arrayLength; i++) {
                     elements[i].classList.remove(activeClass);
                 }
+                // free some mem
+                delete arrayLength;
+                elements = null;
             }
             // add active class to current li
             event.target.parentElement.classList.add([activeClass]);
             let table = event.target.parentElement.id.split("_")[1];
             // get the data for the body
             loadTableToGrid(editableGrid,"data.php",table,"datatable");
+            table = null;
         }
     });
+    return true;
 }
 
 /**
  * base js routing (incoming urls...)
- * TODO: optimize even further...
+ * TODO: optimize even further... (simplification...)
  * @param {*} className the name of the ul class of the menu
  * @param {*} activeClass the class that highlights the current entry
  */
 function routing(className = null, activeClass = null, table_name = "") {
     // unselect currently selected menu entry, if avail
-    let elements = document.querySelectorAll("." + className);
+    let elements = document.getElementsByClassName(className);
     if (elements) {
         const arrayLength = elements.length;
         for (let i = 0; i < arrayLength; i++) {
             elements[i].classList.remove(activeClass);
         }
+        // free em'
+        delete arrayLength;
+        elements = null;
     }
     // activate the correct menu entry
     let table = table_name;
@@ -60,4 +68,7 @@ function routing(className = null, activeClass = null, table_name = "") {
     }
     // activate current entry
     document.getElementById("cur_" + table).classList.add(activeClass);
+    // got ram here too
+    table = null;
+    return true;
 }
